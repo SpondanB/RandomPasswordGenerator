765ec02d2e2e24b93d9c1929b779d89d3982ad22
Spondan Bandyopadhyay <126954192+SpondanB@users.noreply.github.com>
2024-05-11 16:24:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const outputPassword = document.getElementById("outputPassword")
+
+function startFunction(){
+    let length = document.getElementById("length").value
+    let lowercase = document.getElementById("lc").checked
+    let uppercase = document.getElementById("uc").checked
+    let nums = document.getElementById("nc").checked
+    let specialchar = document.getElementById("sc").checked
+
+    let lowercasearr = 'abcdefghijklmnopqrstuvwxyz'
+    let uppercasearr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let numsarr = '0123456789'
+    let specialchararr = '!@#$%^&*()_-+=[]{}:;<>/?'
+
+    let allowedChars = ''
+    let password = ''
+
+    allowedChars += lowercase ? lowercasearr : ''
+    allowedChars += uppercase ? uppercasearr : ''
+    allowedChars += nums ? numsarr : ''
+    allowedChars += specialchar ? specialchararr : ''
+
+    if(allowedChars.length === 0 || length <= 0){
+        outputPassword.innerHTML = 'Enter proper input.'
+    }
+    else{
+        for(let i = 0; i < length; i++){
+            password += allowedChars.charAt(Math.floor(Math.random() * allowedChars.length))
+        }
+        outputPassword.innerHTML = `Your Password is: ${password}`
+    }
+}
